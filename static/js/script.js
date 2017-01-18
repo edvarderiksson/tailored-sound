e4af5fcbfa3f36e7403ab1f1ed5de61dd9b92bba
Andrea Simes <avs2151@columbia.edu>
2017-01-17 22:52:06 -0500

    added new template

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+$("#more-options").click(function(event) {
+    event.preventDefault();
+    if ( $("#hidden-options").css("display") == "none"){
+        $("#hidden-options").css("display", "block");
+        $("#default-button").css("display", "none");
+    }
+    else {
+        $("#hidden-options").css("display", "none");
+        $("#default-button").css("display", "block");
+    }
+    
+});
+
+$("#unchosen-dropdown").click(function(event) {
+    event.preventDefault();
+    var unchosen = $("#unchosen-dropdown").text();
+    var def = $("#default-dropdown").text();
+    $("#default-dropdown").text(unchosen);
+    $("#unchosen-dropdown").text(def);
+});
