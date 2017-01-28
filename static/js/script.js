bc0408d0fec92da76ae848c79f7f0979aa94090a
Andrea Simes <avs2151@columbia.edu>
2017-01-27 22:42:47 -0500

    fixed more options

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,20 @@
 $("#more-options").click(function(event) {
     event.preventDefault();
+    if($("#more-options").text() == "More options"){
+        $("#more-options").text("Fewer options");
+    }
+    else {
+        $("#more-options").text("More options");
+    }
     if ( $("#hidden-options").css("display") == "none"){
         $("#hidden-options").css("display", "block");
         $("#default-button").css("display", "none");
+        console.log($("#more-options").text());
     }
     else {
         $("#hidden-options").css("display", "none");
         $("#default-button").css("display", "block");
+        $("a#more-options").text("More options");
     }
     
 });
