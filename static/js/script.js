532ceea5ccb4c63fce3c5f51405bb2190e8257c2
Andrea Simes <avs2151@columbia.edu>
2017-02-05 22:52:15 -0500

    tweaked details ui

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,7 @@
+/* script file for tailored-sound */
 
-/*$(function () {
-    $('body').show();
-}); */
 
-/*var img = new Image();
-    img.src = '/static/images/trees.png';
-
-var int = setInterval(function() {
-    if (img.complete) {
-        clearInterval(int);
-        document.getElementsByTagName('body')[0].style.backgroundImage = 'url(' + img.src + ')';
-    }
-}, 50);*/
+// handling for displaying load bar
 
 function loading(){
             console.log("hello");
@@ -25,24 +15,20 @@ $("iframe").ready(function() {
 });
 
 
+
+// attempt at loading page only when all elements ready
+// intended to produce cleaner image loading
+
 $(function() {
     $('body').hide();
     $('body').ready(function() { 
-        $('body').show();
-        //alert('ready');        
+        $('body').show();       
     });
 });
-/*
-$('#mood').each(function() {
-    if(this.selected) {
-        $("#hidden-options").hide();
-    }
 
-});*/
 
-//console.log($('#default-dropdown').val());
 
-//console.log($("#default-dropdown option:selected").html());
+// handling for dropdown menu
 
 $("#default-dropdown ").change(showDropdown);
 
@@ -63,61 +49,4 @@ function showDropdown() {
 
 }
 
-
-
-/*if ($('#mood').selected) {
-    console.log("mood");
-    $("#hidden-options").css("display", "none");
-    
-
-
-}
-
-if ($('#word').selected) {
-    console.log("word");
-    $("#hidden-options").css("display", "block");
-    
-
-
-}*/
-
-
-
-
-/*$("#more-options").click(function(event) {
-    event.preventDefault();
-    if($("#more-options").text() == "More options"){
-        $("#more-options").text("Fewer options");
-    }
-    else {
-        $("#more-options").text("More options");
-    }
-    if ( $("#hidden-options").css("display") == "none"){
-        $("#hidden-options").css("display", "block");
-        $("#default-button").css("display", "none");
-        console.log($("#more-options").text());
-    }
-    else {
-        $("#hidden-options").css("display", "none");
-        $("#default-button").css("display", "block");
-        $("a#more-options").text("More options");
-    }
-    
-});*/
-
-/*$("#unchosen-dropdown").click(function(event) {
-    event.preventDefault();
-    var unchosen = $("#unchosen-dropdown").text();
-    var def = $("#default-dropdown").text();
-    $("#default-dropdown").text(unchosen);
-    $("#unchosen-dropdown").text(def);
-});*/
-
-/*$("#unchosen-dropdown").click(function(event) {
-    event.preventDefault();
-    var unchosen = $("#unchosen-dropdown").value();
-    var def = $("#default-dropdown").value();
-    $("#default-dropdown").value(unchosen);
-    $("#unchosen-dropdown").value(def);
-});*/
 
