b4dd439fc567a5d1569398ac636c826f1994bf4f
Andrea Simes <avs2151@columbia.edu>
2017-02-06 00:30:58 -0500

    removed print statement

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def landing_page():
 	elif request.method == 'POST':
 		# dropdown will hold the value 'mood' or 'word'
 		dropdown = request.form['dropdown']
-                print("dropdown:" + type(dropdown))
                 query = request.form['main-input']
 		include = request.form['include']
 		exclude = request.form['exclude']
